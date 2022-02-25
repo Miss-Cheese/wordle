dbf53ed6a9ff0ee347b5262fb24656267b48728b
Miss-Cheese <nat.galligan@gmail.com>
2022-02-25 12:55:44 -0500

    Set up project for the Wordle experiment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Grid from './components/grid'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, it's going to be Wordle!</h1>
       </header>
+      <Grid />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/grid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Square from "./square";
+
+function Grid () {
+    return (
+        <>
+            <Square />
+        </>
+    )
+}
+
+export default Grid
